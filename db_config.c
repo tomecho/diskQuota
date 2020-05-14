617155b744822059896b8a0d910e620834a8b286
Tom Peck <tom@tompeck.me>
2020-05-14 00:25:03 -0400

    try to read columns from db into config struct

--- db_config.c
+++ db_config.c
@@ -1,10 +1,6 @@
 static int directory_callback(void *data, int argc, char **argv, char **azColName){
-  int i;
-  fprintf(stderr, "%s: ", (const char*)data);
-  for(i=0; i<argc; i++){
-    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-  }
-  printf("\n");
+  Config *config = (Config *) data;
+  strcpy(config->directory, argv[1]);
   return 0;
 }
 
@@ -19,28 +15,28 @@ int dbConfig(Config *config, char *file) {
   char *directory_stmt = "SELECT `directory` FROM `web_configs` WHERE `id`=1";
   char *old_stmt = "SELECT `old` FROM `web_configs` WHERE `id`=1";
   char *scan_interval_stmt = "SELECT `scan_interval` FROM `web_configs` WHERE `id`=1";
-  const char* data;
   
   //read directory
-  rc = sqlite3_exec(db,directory_stmt,directory_callback,(void*)data,&(zErrMsg));
+  rc = sqlite3_exec(db,directory_stmt,directory_callback, &config, &zErrMsg);
   if(rc != SQLITE_OK){
     fprintf(stderr, "failed to read database directory: %s \n", zErrMsg);
     sqlite3_free(zErrMsg);
   }
+  printf("%s", config->directory);
 
-  //read old
-  rc = sqlite3_exec(db,old_stmt,directory_callback,(void*)data,&(zErrMsg));
-  if(rc != SQLITE_OK){
-    fprintf(stderr, "failed to read database old: %s \n",zErrMsg);
-    sqlite3_free(zErrMsg);
-  }
+  // //read old
+  // rc = sqlite3_exec(db,old_stmt,directory_callback,(void*)data,&(zErrMsg));
+  // if(rc != SQLITE_OK){
+  //   fprintf(stderr, "failed to read database old: %s \n",zErrMsg);
+  //   sqlite3_free(zErrMsg);
+  // }
   
-  //read scan_interval
-  rc = sqlite3_exec(db,scan_interval_stmt,directory_callback,(void*)data,&(zErrMsg));
-  if(rc != SQLITE_OK){
-    fprintf(stderr, "failed to read database scan_interval: %s \n", zErrMsg);
-    sqlite3_free(zErrMsg);
-  }
+  // //read scan_interval
+  // rc = sqlite3_exec(db,scan_interval_stmt,directory_callback,(void*)data,&(zErrMsg));
+  // if(rc != SQLITE_OK){
+  //   fprintf(stderr, "failed to read database scan_interval: %s \n", zErrMsg);
+  //   sqlite3_free(zErrMsg);
+  // }
 
   sqlite3_close(db);
   return 1;
